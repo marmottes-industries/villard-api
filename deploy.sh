78bd9866bd12254c406141134e096fb3452f32e8
antonin.p <antonin.p@lahautesociete.com>
2026-06-08 14:59:59 +0200

    feat: add deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+git fetch --tags
+# shellcheck disable=SC2046
+git checkout $(git tag --sort=-version:refname | head -1)
+composer install --no-dev --optimize-autoloader
+php bin/console doctrine:migrations:migrate --no-interaction --env=prod
+php bin/console cache:clear --env=prod
+echo "Deploy terminé."
